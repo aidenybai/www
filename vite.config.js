d0c826f93f0c8f06d3c1dd3f7341754ac5185430
Aiden Bai <aiden.bai05@gmail.com>
2021-08-29 19:47:23 -0700

    remove plugins

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,9 @@
 import { defineConfig } from 'vite';
-import { minifyHtml } from 'vite-plugin-html';
-import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
   esbuild: {
     jsxFactory: 'jsx',
     jsxFragment: 'Fragment',
     jsxInject: `import { jsx, jsxs, Fragment } from 'million/jsx-runtime'`,
-  },
-  plugins: [minifyHtml(), viteSingleFile()],
-  build: {
-    target: 'esnext',
-    assetsInlineLimit: 100000000,
-    chunkSizeWarningLimit: 100000000,
-    cssCodeSplit: false,
-    brotliSize: false,
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => 'everything.js',
-      },
-    },
   },
 });
